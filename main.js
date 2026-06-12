a2f8bce2987b0f88d55cc97b9c420475663044b9
Alexander <ramonalaurencio5@gmail.com>
2026-06-12 14:27:04 -0400

    Adding vibration when inputs are empty

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ inputButton.addEventListener("click", (event) =>{
     let password = inputPassword.value.trim();
 
     if (!user || !email || !password){
-        alert("Fill all the fields")
+        alert("Fill all the fields");
+        navigator.vibrate(200, 100, 200);
     } else (
         alert("You've been registered successfully")
     )
