1746410c3570c1d36c7038f2e4cdd645b5a0f183
Alexander <ramonalaurencio5@gmail.com>
2026-06-12 14:32:14 -0400

    sdsqddadadadas

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ inputButton.addEventListener("click", (event) =>{
     let password = inputPassword.value.trim();
 
     if (!user || !email || !password){
-        alert("Changes applied bitch");
+        alert("Fill all the fields");
         navigator.vibrate(200, 100, 200);
     } else (
         alert("You've been registered successfully")
